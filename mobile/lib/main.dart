a39170b01b089c80012c29247de2b1b6ebbf600b
Saad Ismail <saad3112@gmail.com>
2019-04-27 21:24:54 +0500

    snowball app implementation (not structured)

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,113 +1,227 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:contacts_service/contacts_service.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:sms/sms.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+    return new MaterialApp(
+      title: 'On Air',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new InviteScreen(title: 'On Air')
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class InviteScreen extends StatefulWidget {
+  InviteScreen({Key key, this.title}) : super(key: key);
 
   final String title;
+  final String cancelLabel = 'Cancel';
+  final String selectLabel = 'Select contacts';
+  final Color floatingButtonColor = Colors.red;
+  final IconData reloadIcon = Icons.refresh;
+  final IconData fireIcon = Icons.done_outline;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _InviteScreenState createState() => new _InviteScreenState(
+        floatingButtonLabel: this.selectLabel,
+        icon: this.fireIcon,
+        floatingButtonColor: this.floatingButtonColor,
+      );
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _InviteScreenState extends State<InviteScreen> {
+  List<Contact> _contacts = new List<Contact>();
+  List<CustomContact> _uiCustomContacts = List<CustomContact>();
+  List<CustomContact> _allContacts = List<CustomContact>();
+  bool _isLoading = false;
+  bool _isSelectedContactsView = false;
+  String floatingButtonLabel;
+  Color floatingButtonColor;
+  IconData icon;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  _InviteScreenState({
+    this.floatingButtonLabel,
+    this.icon,
+    this.floatingButtonColor,
+  });
+
+  @override
+  void initState() {
+    super.initState();
+    getContactsPermission().then((granted) {
+      if (granted[PermissionGroup.contacts] == PermissionStatus.granted) {
+        refreshContacts();
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text('Oops!'),
+                content: const Text('Looks like permission to read contacts is not granted.'),
+                actions: <Widget>[
+                  FlatButton(
+                    child: const Text('OK'),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ],
+              ),
+        );
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    print("THIS ran");
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return new Scaffold(
+      appBar: (_isSelectedContactsView) ? 
+        new AppBar(
+          title: new Text(widget.title),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {
+                _sendSms();
+              },
+            )
+          ]
+        ) : new AppBar(title: new Text(widget.title)),
+      body: !_isLoading
+          ? Container(
+              child: ListView.builder(
+                itemCount: _uiCustomContacts?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  CustomContact _contact = _uiCustomContacts[index];
+                  var _phonesList = _contact.contact.phones.toList();
+
+                  return _buildListTile(_contact, _phonesList);
+                },
+              ),
+            )
+          : Center(
+              child: CircularProgressIndicator(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      floatingActionButton: new FloatingActionButton.extended(
+        backgroundColor: floatingButtonColor,
+        onPressed: _onSubmit,
+        icon: Icon(icon),
+        label: Text(floatingButtonLabel),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
+
+  void _sendSms() {
+    setState(() {
+      _isLoading = true;
+    });
+
+    SmsSender sender = new SmsSender();
+    for (CustomContact contact in _uiCustomContacts) {
+      SmsMessage message = new SmsMessage(contact.contact.phones.elementAt(0).value, 'Hello flutter!');
+      sender.sendSms(message);
+      contact.isChecked = false;
+    }
+
+    setState(() {
+      _isLoading = false;
+      _uiCustomContacts = _allContacts;
+      _isSelectedContactsView = false;
+      _restateFloatingButton(
+        widget.selectLabel,
+        Icons.done_outline,
+        Colors.red,
+      );
+    });
+  }
+
+  void _onSubmit() {
+    setState(() {
+      if (!_isSelectedContactsView) {
+        _uiCustomContacts =
+            _allContacts.where((contact) => contact.isChecked == true).toList();
+        _isSelectedContactsView = true;
+        _restateFloatingButton(
+          widget.cancelLabel,
+          Icons.refresh,
+          Colors.green,
+        );
+      } else {
+        _uiCustomContacts = _allContacts;
+        _isSelectedContactsView = false;
+        _restateFloatingButton(
+          widget.selectLabel,
+          Icons.done_outline,
+          Colors.red,
+        );
+      }
+    });
+  }
+
+  ListTile _buildListTile(CustomContact c, List<Item> list) {
+    return ListTile(
+      // leading: CircleAvatar(
+      //         child: Text(
+      //             (c.contact.displayName[0] +
+      //                 c.contact.displayName[1].toUpperCase()),
+      //             style: TextStyle(color: Colors.white)),
+      //       ),
+      title: Text(c.contact.displayName ?? ""),
+      subtitle: list.length >= 1 && list[0]?.value != null
+          ? Text(list[0].value)
+          : Text(''),
+      trailing: Checkbox(
+          activeColor: Colors.green,
+          value: c.isChecked,
+          onChanged: (bool value) {
+            setState(() {
+              c.isChecked = value;
+            });
+          }),
+    );
+  }
+
+  void _restateFloatingButton(String label, IconData icon, Color color) {
+    floatingButtonLabel = label;
+    this.icon = icon;
+    floatingButtonColor = color;
+  }
+
+  refreshContacts() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var contacts = await ContactsService.getContacts();
+    _populateContacts(contacts);
+  }
+
+  void _populateContacts(Iterable<Contact> contacts) {
+    _contacts = contacts.where((item) => item.displayName != null).toList();
+    _contacts.sort((a, b) => a.displayName.compareTo(b.displayName));
+    _allContacts =
+        _contacts.map((contact) => CustomContact(contact: contact)).toList();
+    setState(() {
+      _uiCustomContacts = _allContacts;
+      _isLoading = false;
+    });
+  }
+
+  Future< Map<PermissionGroup, PermissionStatus> > getContactsPermission() =>
+      PermissionHandler().requestPermissions([PermissionGroup.contacts]);
+}
+
+class CustomContact {
+  final Contact contact;
+  bool isChecked;
+
+  CustomContact({
+    this.contact,
+    this.isChecked = false,
+  });
 }
