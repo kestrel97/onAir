f21630932c64a7a9e2719441969bb133ad6e1fe6
Saad Ismail <saad3112@gmail.com>
2019-05-03 22:11:23 +0500

    include image_link in response model

--- web/models/response.model.js
+++ web/models/response.model.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const schema = new Schema({
     sender: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     question: { type: Schema.Types.ObjectId, ref: 'Question', required: true },
-    response: { type: String, required: true }
+    response: { type: String, required: true },
+    image_link: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Response', schema);
